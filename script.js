ad782f6cb1f2d44e5f9585a228b4f4a7b6ec4fd9
balonbao247 <tonlegiabao@gmail.com>
2021-07-03 15:56:22 +0700

    full js

--- script.js
+++ script.js
@@ -29,12 +29,17 @@ const menu = [
 }
 ]
 const sectionCenter = document.querySelector(".section-center")
+const btnContainer = document.querySelector(".btn-container")
 
 window.addEventListener("DOMContentLoaded", function () {
-  let displayMenu = menu.map(function (item) {
+  diplayMenuItems(menu)
+  displayMenuButtons()
+});
 
+function diplayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
     return `<article class="menu-item">
-          <img src=${item.img} alt=${item.title} width="250px" height="150px" class="photo" />
+          <img src=${item.img}  width="250px" height="150px" alt=${item.title} class="photo" />
           <div class="item-info">
             <header>
               <h4>${item.title}</h4>
@@ -47,7 +52,44 @@ window.addEventListener("DOMContentLoaded", function () {
         </article>`
   });
   displayMenu = displayMenu.join("")
-  console.log(displayMenu)
-
   sectionCenter.innerHTML = displayMenu
-});
+}
+function displayMenuButtons() {
+  const categories = menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category)
+      }
+      return values
+    },
+    ["all"]
+  );
+  const categoryBtns = categories
+    .map(function (category) {
+      return `<button type="button" class="filter-btn" data-id=${category}>
+          ${category}
+        </button>`
+    })
+    .join("")
+
+  btnContainer.innerHTML = categoryBtns
+  const filterBtns = btnContainer.querySelectorAll(".filter-btn")
+  console.log(filterBtns)
+
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      })
+      if (category === "all") {
+        diplayMenuItems(menu)
+      } else {
+        diplayMenuItems(menuCategory)
+      }
+    })
+  })
+}
